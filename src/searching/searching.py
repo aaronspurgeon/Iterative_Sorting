7a09434e20f26144a05cedd967ca709efd6f0a81
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-07-07 20:44:50 -0400

    Completed binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -13,5 +13,17 @@ def linear_search(arr, target):
 
 
 def binary_search(arr, target):
+    # Your code here
+    high = len(arr) - 1
+    low = 0
+    while low <= high:
+        middle = (high + low) // 2
+        if arr[middle] == target:
+            return middle
+        else:
+            if arr[middle] > target:
+                high = middle - 1
+            elif arr[middle] < target:
+                low = middle + 1
 
     return -1  # not found
