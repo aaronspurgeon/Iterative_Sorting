088600bcaf39ef84efb0d02bc9854d59644ed7e6
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-07-07 20:44:23 -0400

    Completed linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,17 @@
 def linear_search(arr, target):
     # Your code here
-
+    index = 0
+    for i in arr:
+        if i == target:
+            return index
+        else:
+            index += 1
 
     return -1   # not found
 
-
 # Write an iterative implementation of Binary Search
+
+
 def binary_search(arr, target):
-
-    # Your code here
-
 
     return -1  # not found
